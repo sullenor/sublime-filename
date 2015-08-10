eb06874f0a619c161c8ee26b164b265150f7bfeb
Trdat Mkrtchyan <zeffirsky@yandex-team.ru>
2015-08-10 18:11:07 +0300

    Added selection feature

--- filename.py
+++ filename.py
@@ -1,14 +1,23 @@
 import os
 import sublime, sublime_plugin
 
-class FilenameCommand(sublime_plugin.TextCommand):
+class FilenameCommand(sublime_plugin.TextCommand):  
   def run(self, edit):
     fn = self.view.file_name()
+    sel = self.view.sel()
 
-    if fn:
+    if fn :
       # Оставляем только имя файла
       fn = os.path.splitext(fn)[0]
-      # Позиция курсора
-      pos = self.view.sel()[0].begin()
+    else :
+      fn = "non-selected-file"
 
+    # если выбран текст
+    if len(sel) > 0 :
+      for region in sel:
+        self.view.replace(edit, region, fn)
+
+    else : 
+      # Позиция курсора
+      pos = sel[0].begin()
       self.view.insert(edit, pos, fn)
