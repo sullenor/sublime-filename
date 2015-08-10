ebe3dc7b575a77ff61d989aa5a64bf0651e9c611
Trdat Mkrtchyan <zeffirsky@yandex-team.ru>
2015-08-10 21:03:38 +0300

    Small fixes

--- filename.py
+++ filename.py
@@ -7,18 +7,18 @@ class FilenameCommand(sublime_plugin.TextCommand):
     fn = self.view.file_name()
     sel = self.view.sel()
 
-    if fn :
+    if fn:
       # Оставляем только имя файла
       fn = basename(os.path.splitext(fn)[0])
-    else :
-      fn = "Error: select file"
+    else:
+      fn = "Error: file buffer is empty"
 
-    # если выбран текст
-    if len(sel) > 0 :
+    if len(sel) > 0:
+      # если выбран текст - заменяем каждый элемент
       for region in sel:
         self.view.replace(edit, region, fn)
 
-    else : 
+    else: 
       # Позиция курсора
       pos = sel[0].begin()
       self.view.insert(edit, pos, fn)
